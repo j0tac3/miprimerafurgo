f44aacc1b64342e4d8682d5d6c6453c78d45e8e6
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2021-06-08 14:15:04 +0200

    Maquetando la vista del post seleccionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-primera-furgo-angular';
+
+  onActivate(event : Event){
+    window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
+  }
 }
