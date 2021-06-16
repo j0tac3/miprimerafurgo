06a4c2db998b183cf8c1585c36567e63785550e5
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2021-06-16 15:28:15 +0200

    Dando funcionalidad a las sugerencias del buscador

--- src/app/pages/articulos/articulos.component.ts
+++ src/app/pages/articulos/articulos.component.ts
@@ -11,6 +11,8 @@ export class ArticulosComponent implements OnInit {
   public articulos : any = ['https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=miprimerafurg-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B07MQBGCLF&linkId=b7118fa76bc622117a477805d0044de1&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr',
                             'https://rcm-eu.amazon-adsystem.com/e/cm?ref=qf_sp_asin_til&t=miprimerafurg-21&m=amazon&o=30&p=8&l=as1&IS1=1&asins=B07MQBGCLF&linkId=b7118fa76bc622117a477805d0044de1&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr'];
   public formArticulos!: FormGroup;
+  public themes = [{'desc':'Viajes'}, {'desc':'Playa'}, {'desc':'Montaña'}, {'desc':'Verano'}, {'desc':'Escapada'}];
+  public themesSelected : any;
 
   constructor(  private fb : FormBuilder,
                 private sanitizer: DomSanitizer ) { }
@@ -40,6 +42,19 @@ export class ArticulosComponent implements OnInit {
 
   cargarArticulo(articulo : string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(articulo)
+  }
+
+  displayMaches( event : Event) {
+    console.log(event);
+    //const matchedArray = this.shearchTheme(event.target.value, this.themes);
+  }
+
+  shearchTheme( wordToSearch : string , themes : object) {
+    //return this.themes.filter( theme => {
+    this.themesSelected = this.themes.filter( theme => {
+      const regex = new RegExp(wordToSearch, 'gi');
+      return theme.desc.match(regex);
+    })
   }
 
 }
