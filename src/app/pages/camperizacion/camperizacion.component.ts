07bfd96b4f078647c1fc9a7df9a22abffc935416
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2021-06-17 12:27:20 +0200

    Diseñando el formulario de contacto

--- src/app/pages/camperizacion/camperizacion.component.ts
+++ src/app/pages/camperizacion/camperizacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-camperizacion',
@@ -7,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamperizacionComponent implements OnInit {
 public temas = ['Sugerencia', 'Fallo en Diseño', 'Error de funcionamiento', 'Otros'];
+public formContacto! : FormGroup;
 
-  constructor() { }
+  constructor( private fb : FormBuilder ) { }
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit(){
+    this.formContacto = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', Validators.required],
+      tema: ['', Validators.required],
+      observacion: ['', Validators.required]
+    })
   }
 
 }
