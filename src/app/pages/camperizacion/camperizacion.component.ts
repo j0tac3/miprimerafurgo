5c08be4f62980db0ce528d437b9c359d16efa902
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2021-06-21 14:01:37 +0200

    Diseñando el formulario de contacto

--- src/app/pages/camperizacion/camperizacion.component.ts
+++ src/app/pages/camperizacion/camperizacion.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./camperizacion.component.css']
 })
 export class CamperizacionComponent implements OnInit {
+  public subOptionsTextVisible : boolean = false;
+  public subOptionsMediaVisible : boolean = false;
 
   constructor(  ) { }
 
   ngOnInit(): void {
+  }
+
+  showSuboption( suboptionName : string) : void {
+    if (suboptionName === ' texto') {
+      this.subOptionsTextVisible = !this.subOptionsTextVisible;
+    } else if (suboptionName === ' media'){
+      this.subOptionsMediaVisible = !this.subOptionsMediaVisible;
+    }
   }
 
 }
